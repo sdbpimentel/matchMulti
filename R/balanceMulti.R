94cca9239f09fbbc14a4d435006996d5fb1e8351
Sam Pimentel <sdbpimentel@gmail.com>
2021-12-06 13:08:00 -0800

    Initial commit.

---
+++ R/balanceMulti.R
@@ -0,0 +1,26 @@
+balanceMulti <-
+function(match.obj, student.cov = NULL, school.cov = NULL){
+	if(is.null(match.obj)){
+		 warning('match.obj has value NULL.  Did you make sure optmatch was available before generating matched samples?')
+		return(NULL)
+	}
+	treatment <- match.obj$treatment
+	
+	#if no student covariates are provided, compute balance on all
+	if (is.null(student.cov)) student.cov = setdiff(colnames(match.obj$raw), c(treatment, match.obj$school.id))
+	
+	#student balance
+	student.bal <- balanceTable(match.obj$raw[c(student.cov,treatment)], match.obj$matched[c(student.cov, treatment)], treatment)
+	
+	#school balance
+	school.bal <- NULL
+	if(!is.null(school.cov)){
+		schools.raw <- students2schools(match.obj$raw, c(school.cov, treatment), match.obj$school.id)
+		schools.matched <- schools.raw[which(schools.raw[[match.obj$school.id]] %in% as.vector(match.obj$school.match)),]
+		
+		id.idx <- which(colnames(schools.raw) == match.obj$school.id)
+	
+		school.bal <- balanceTable(schools.raw[-id.idx], schools.matched[-id.idx], treatment)
+	}
+	return(list('students' = student.bal, 'schools' = school.bal))
+}
